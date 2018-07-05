805482750f6497ab8bcf1f196444a3985c77fcb6
Joey Jiron <joeyjiron06@gmail.com>
2018-07-05 12:08:15 -0700

    improved login experience

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import { Button } from '@material-ui/core';
+import { Button, CircularProgress } from '@material-ui/core';
 import './index.css';
 import * as firebase from 'firebase';
 
 class Login extends Component {
   componentWillMount() {
+    this.setState({
+      isReturningUser: JSON.parse(localStorage.getItem('isReturningUser'))
+    });
+
     this.unsubscribeAuthStateChanged = firebase
       .auth()
       .onAuthStateChanged(user => {
+        if (!user) {
+          return;
+        }
+
         firebase.user = user;
         firebase.user.db = firebase.database().ref(user.uid);
         localStorage.setItem('user', JSON.stringify(user));
@@ -28,12 +36,15 @@ class Login extends Component {
   }
 
   async login() {
+    localStorage.setItem('isReturningUser', true);
     firebase
       .auth()
       .signInWithRedirect(new firebase.auth.FacebookAuthProvider());
   }
 
   render() {
+    const { isReturningUser } = this.state;
+
     return (
       <div className="login-page">
         <div className="login-page-background-container">
@@ -48,9 +59,15 @@ class Login extends Component {
         <div className="login-page-content">
           <div className="login-page-title">Food Journal</div>
           <p className="login-page-subtitle">keeping track of what you eat</p>
-          <Button variant="contained" onClick={this.login}>
+          <Button
+            variant="contained"
+            onClick={this.login}
+            disabled={!!isReturningUser}
+          >
             Login with facebook
           </Button>
+
+          {isReturningUser ? <CircularProgress color="secondary" /> : null}
         </div>
       </div>
     );
--- src/routes/settings/index.js
+++ src/routes/settings/index.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import { Button, Typography } from '@material-ui/core';
+import firebase from 'firebase';
+import './index.css';
 
 class SettingsPage extends Component {
+  handleSignout() {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        localStorage.clear();
+        window.location.href = window.location.href; // refresh
+      })
+      .catch(error => {
+        console.error('error signing out', error);
+      });
+  }
+
   render() {
     return (
-      <div>
-        <h1>Settings</h1>
+      <div className="settings-page">
+        <Typography variant="display1" gutterBottom>
+          Settings
+        </Typography>
+
+        <Button variant="raised" color="secondary" onClick={this.handleSignout}>
+          Sign out
+        </Button>
       </div>
     );
   }
