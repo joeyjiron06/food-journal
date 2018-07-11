c836de777bc4b02bf4b69130cd388d5ac719e568
Joey Jiron <joeyjiron06@gmail.com>
2018-07-10 19:01:19 -0700

    added goal and calculation for meals

--- src/routes/settings/index.js
+++ src/routes/settings/index.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
-import { Button, Typography } from '@material-ui/core';
-import { auth } from 'firebase';
+import { Button, Typography, Menu, MenuItem } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import Slider from '@material-ui/lab/Slider';
+import { auth, database } from 'firebase';
+import debounce from '../../utils/debouncer';
 import './index.css';
+
+const styles = {
+  goalContainer: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  goalPercentage: {
+    marginRight: 10
+  },
+  goalMealType: {
+    flexShrink: 0
+  }
+};
 
 class SettingsPage extends Component {
   state = {
-    openPrivacyPolicy: false,
-    openTOS: false
+    goalValue: 0,
+    mealTypeButtonRef: null
+  };
+
+  handleGoalPercentageChanged = (event, value) => {
+    this.setGoalPercentageDebouncer(value);
+    this.setState({ goalPercentage: value });
+  };
+  handleMealTypeClick = event => {
+    this.setState({ mealTypeButtonRef: event.target });
+  };
+  handleMealTypeMenuClose = () => {
+    this.setState({ mealTypeButtonRef: null });
+  };
+  handleMealTypeSelected = event => {
+    const value = event.target.getAttribute('value');
+    const title = event.target.textContent;
+    this.setState({ mealTypeButtonRef: null, goalMealType: title });
+    this.goalRef.child('mealType').set(value);
   };
 
   handleSignout() {
@@ -21,12 +54,98 @@ class SettingsPage extends Component {
       });
   }
 
+  UNSAFE_componentWillMount() {
+    this.mounted = true;
+    this.goalRef = database().user.child('goal');
+    this.goalRef
+      .once('value')
+      .then(snapshot => {
+        if (!this.mounted) {
+          return;
+        }
+
+        const goal = snapshot.val();
+        if (goal) {
+          this.setState({
+            goalPercentage: goal.percentage,
+            goalMealType: goal.mealType
+          });
+        }
+      })
+      .catch(error => {
+        if (!this.mounted) {
+          return;
+        }
+        console.error('error fetching goal');
+      });
+
+    this.setGoalPercentageDebouncer = debounce(500, percentage => {
+      percentage = Math.round(percentage);
+      console.log('setting goal percentage to', percentage);
+      this.goalRef.child('percentage').set(percentage);
+    });
+  }
+  UNSAFE_componentWillUnmount() {
+    this.mounted = false;
+    this.setGoalPercentageDebouncer.destroy();
+  }
+
   render() {
+    const { goalPercentage, goalMealType, mealTypeButtonRef } = this.state;
+    const { classes } = this.props;
+
     return (
       <div className="settings-page">
         <Typography variant="display1" gutterBottom>
           Settings
         </Typography>
+
+        <div>
+          <Typography>
+            What percentage of a specific kind of meal type would you like as
+            your goal?
+          </Typography>
+
+          <div className={classes.goalContainer}>
+            <Slider
+              style={{ marginRight: 20 }}
+              value={goalPercentage || 0}
+              onChange={this.handleGoalPercentageChanged}
+            />
+            <Typography
+              variant="subheading"
+              className={classes.goalPercentage}
+            >{`${Math.round(goalPercentage)}%`}</Typography>
+
+            <Button
+              onClick={this.handleMealTypeClick}
+              className={classes.goalMealType}
+            >
+              {goalMealType ? goalMealType : 'Meal Type'}
+            </Button>
+            <Menu
+              anchorEl={mealTypeButtonRef}
+              open={Boolean(mealTypeButtonRef)}
+              onClose={this.handleMealTypeMenuClose}
+            >
+              <MenuItem value="junk" onClick={this.handleMealTypeSelected}>
+                Junk Food
+              </MenuItem>
+              <MenuItem value="meat" onClick={this.handleMealTypeSelected}>
+                Meat
+              </MenuItem>
+              <MenuItem value="vegan" onClick={this.handleMealTypeSelected}>
+                Vegan
+              </MenuItem>
+              <MenuItem
+                value="vegetarian"
+                onClick={this.handleMealTypeSelected}
+              >
+                Vegetarian
+              </MenuItem>
+            </Menu>
+          </div>
+        </div>
 
         <div className="settings-page-link-container">
           <a
@@ -54,4 +173,4 @@ class SettingsPage extends Component {
   }
 }
 
-export default SettingsPage;
+export default withStyles(styles)(SettingsPage);
--- src/routes/statistics/index.js
+++ src/routes/statistics/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Typography, Tabs, Tab } from '@material-ui/core';
+import { database } from 'firebase';
 import { fetchMeals } from '../../model/meal';
 import {
   dateRangeOfToday,
@@ -7,8 +8,11 @@ import {
   dateRangeOfMonth,
   dateRangeOfYear
 } from '../../model/date';
-
 import './index.css';
+
+const capitalizeFirstLetter = str => {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
 
 const calculateStats = meals => {
   meals = meals || [];
@@ -72,7 +76,7 @@ class StatisticsPage extends Component {
     value: 0
   };
 
-  loadMeals(dateRange, cacheKey) {
+  loadMeals(dateRange, cacheKey, onComplete) {
     if (this.cancelLoading) {
       this.cancelLoading();
       this.cancelLoading = null;
@@ -100,6 +104,10 @@ class StatisticsPage extends Component {
         isLoading: false
       });
       this[cacheKey] = meals;
+
+      if (typeof onComplete === 'function') {
+        onComplete(error, meals);
+      }
     });
   }
 
@@ -115,7 +123,50 @@ class StatisticsPage extends Component {
         this.loadMeals(dateRangeOfMonth(), 'month');
         break;
       case 3:
-        this.loadMeals(dateRangeOfYear(), 'year');
+        this.loadMeals(dateRangeOfYear(), 'year', (error, meals) => {
+          if (this.state.goal) {
+            return;
+          }
+
+          database()
+            .user.child('goal')
+            .once('value')
+            .then(snapshot => {
+              if (!this.mounted) {
+                return;
+              }
+
+              const goal = snapshot.val();
+              if (goal) {
+                const numMealsTowardGoal = meals.filter(
+                  meal => meal.type === goal.mealType
+                ).length;
+                console.log(
+                  'numMealsTowardGoal',
+                  numMealsTowardGoal,
+                  meals.length,
+                  goal
+                );
+
+                const numMealsToReachGoal = Math.round(
+                  ((goal.percentage / 100) * meals.length -
+                    numMealsTowardGoal) /
+                    (1 - goal.percentage / 100)
+                );
+
+                this.setState({
+                  numMealsToReachGoal,
+                  goal
+                });
+              }
+            })
+            .catch(error => {
+              if (!this.mounted) {
+                return;
+              }
+              console.error('error fetching goal', error);
+            });
+        });
         break;
       default:
         console.warn('unhandled value', value);
@@ -126,12 +177,16 @@ class StatisticsPage extends Component {
   };
 
   UNSAFE_componentWillMount() {
+    this.mounted = true;
     this.handleTabClicked(null, 0);
+  }
+  UNSAFE_componentWillUnMount() {
+    this.mounted = false;
   }
 
   render() {
     const stats = this.state.stats || {};
-    const { value } = this.state;
+    const { value, goal, numMealsToReachGoal } = this.state;
 
     return (
       <div className="stats-page">
@@ -159,6 +214,14 @@ class StatisticsPage extends Component {
           <Statistic title="Vegan" value={stats.vegan} />
           <Statistic title="Vegetarian" value={stats.vegetarian} />
         </div>
+
+        {goal && numMealsToReachGoal !== undefined ? (
+          <Typography gutterBottom>
+            {`${numMealsToReachGoal} meals to reach ${
+              goal.percentage
+            }% ${capitalizeFirstLetter(goal.mealType)}`}
+          </Typography>
+        ) : null}
       </div>
     );
   }
